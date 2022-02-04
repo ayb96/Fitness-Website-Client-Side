89911b11a5fe2493c725b2ff79b5631b31b6daa9
hussein <hussein-ayoub-96@hotmail.com>
2022-02-04 16:44:25 +0200

    Adding Choose Us Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./scss/style.css";
 import Header from "./component/Header";
 import Category from "./component/Category";
 import Reviews from "./component/Reviews";
+import ChooseUs from "./component/ChooseUs";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Category/>
-      <Reviews/>
+      <Category />
+      <Reviews />
+      <ChooseUs />
     </div>
   );
 }
---
+++ src/component/ChooseUs.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function ChooseUs() {
+  return (
+    <div className="chooseus">
+      <div className="main-content">
+        <div className="box1">WHY CHOOSE US</div>
+        <div className="line1"></div>
+        <div className="box2">FEATURES</div>
+        <div className="box3">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
+        </div>
+        <div className="box4">
+          <div className="tops">
+            <div className="tops1">TOPS</div>
+            <div className="tops2"></div>
+            <div className="tops3">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor.
+            </div>
+          </div>
+          <div className="bottoms">
+            <div className="bottoms1">BOTTOMS</div>
+            <div className="bottoms2"></div>
+            <div className="bottoms3">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor.
+            </div>
+          </div>
+          <div className="hoodies">
+            <div className="hoodies1">HOODIES AND JACKETS</div>
+            <div className="hoodies2"></div>
+            <div className="hoodies3">HOODIES AND JACKETS</div>
+          </div>
+          <div className="accessories">
+            <div className="accessories1">ACCESSORIES</div>
+            <div className="accessories2"></div>
+            <div className="accessories3">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

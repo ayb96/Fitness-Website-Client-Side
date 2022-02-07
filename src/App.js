c31df058a35bdc0ea10f80c014273014054edaa8
hussein <hussein-ayoub-96@hotmail.com>
2022-02-07 23:30:48 +0200

    Fix Header and Category Sections

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Slider /> */}
-      {/* <Category /> */}
+      <Slider />
+      <Category />
       <Reviews />
       <ChooseUs />
       <Video />
--- src/component/Header.js
+++ src/component/Header.js
@@ -19,89 +19,90 @@ export default function Header() {
       <div className="header-logo">
         <img src={logoIcon} alt="LOGO" />
       </div>
-      <div className="top-nav-header">
-        <div className="top-header">
-          <div className="info-header">
-            <div className="info-header1">
-              <div>
-                <PhoneEnabledIcon />
-              </div>
-              <div>740.993.7266</div>
+      {/* <div className="top-nav-header"> */}
+      <div className="top-header">
+        <div className="info-header">
+          <div className="info-header1">
+            <div>
+              <PhoneEnabledIcon />
             </div>
-            <div className="info-header2">
-              <div>
-                <MailOutlineOutlinedIcon />
-              </div>
-              <div>Shanny90@gmail.com</div>
-            </div>
+            <div>740.993.7266</div>
           </div>
-          <div className="social-icon">
-            <div className="facbook-icon">
-              <FacebookIcon />
+          <div className="info-header2">
+            <div>
+              <MailOutlineOutlinedIcon />
             </div>
-            <div className="youtube-icon">
-              <YouTubeIcon />
-            </div>
-            <div className="gmail-icon">
-              <GoogleIcon />
-            </div>
-            <div className="instagram-icon">
-              <InstagramIcon />
-            </div>
+            <div>Shanny90@gmail.com</div>
           </div>
         </div>
-        <div className="nav-header">
-          <nav className="navbar">
-            <div className="navbar-links">
-              <ul>
-                <li>
-                  <a href="#" className="home">
-                    HOME
-                  </a>
-                </li>
-                <li>
-                  <a href="#">SHOP</a>
-                </li>
-                <li>
-                  <a href="#">ABOUT</a>
-                </li>
-                <li>
-                  <a href="#">CONTACT</a>
-                </li>
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-              </ul>
-            </div>
-            <div style={{ display: "flex" }}>
-              <div
-                className="search-title"
-                style={{
-                  display: "block",
-                  padding: "1rem",
-                  borderLeft: "1px #D1D1D1 solid",
-                  borderBottom: "1px #D1D1D1 solid",
-                  cursor: "pointer",
-                }}
-              >
-                <SearchIcon />
-              </div>
-              <div
-                className="toggle-button"
-                style={{
-                  display: "block",
-                  padding: "1rem",
-                  backgroundColor: "#E41C26",
-                  cursor: "pointer",
-                }}
-                onClick={runMe}
-              >
-                <img src={sideBarIcon} alt="" />
-              </div>
-            </div>
-          </nav>
+        <div className="social-icon">
+          <div className="facbook-icon">
+            <FacebookIcon />
+          </div>
+          <div className="youtube-icon">
+            <YouTubeIcon />
+          </div>
+          <div className="gmail-icon">
+            <GoogleIcon />
+          </div>
+          <div className="instagram-icon">
+            <InstagramIcon />
+          </div>
         </div>
       </div>
+      <div></div>
+      <div className="nav-header">
+        <nav className="navbar">
+          <div className="navbar-links">
+            <ul>
+              <li>
+                <a href="#" className="home">
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a href="#">SHOP</a>
+              </li>
+              <li>
+                <a href="#">ABOUT</a>
+              </li>
+              <li>
+                <a href="#">CONTACT</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <div style={{ display: "flex" }}>
+            <div
+              className="search-title"
+              style={{
+                display: "block",
+                padding: "1rem",
+                borderLeft: "1px #D1D1D1 solid",
+                borderBottom: "1px #D1D1D1 solid",
+                cursor: "pointer",
+              }}
+            >
+              <SearchIcon />
+            </div>
+            <div
+              className="toggle-button"
+              style={{
+                display: "block",
+                padding: "1rem",
+                backgroundColor: "#E41C26",
+                cursor: "pointer",
+              }}
+              onClick={runMe}
+            >
+              <img src={sideBarIcon} alt="" />
+            </div>
+          </div>
+        </nav>
+      </div>
+      {/* </div> */}
     </div>
   );
 }

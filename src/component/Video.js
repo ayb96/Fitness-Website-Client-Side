d83efaff9175aeae872a32ed35ddcc0e5e8980b0
hussein <hussein-ayoub-96@hotmail.com>
2022-02-10 15:16:49 +0200

    some update

--- src/component/Video.js
+++ src/component/Video.js
@@ -1,7 +1,7 @@
 import React from "react";
 import gridpic from "../Images/Group 87.svg";
-import normalpic from "../Images/Rectangle 28.png";
-import { Player } from "video-react";
+
+import ReactPlayer from "react-player";
 export default function Video() {
   return (
     <div className="video">
@@ -12,10 +12,19 @@ export default function Video() {
         <img src={gridpic} alt="" />
       </div>
       <div className="video3">
-        <div className="video3-content">
+        {/* <div className="video3-content">
           <Player className="video-player">
             <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
           </Player>
+        </div> */}
+        <div className="video3-content">
+          <ReactPlayer
+            className="video-player"
+            url="https://youtu.be/CH50zuS8DD0"
+            width="100%"
+            height="100%"
+            controls={true}
+          />
         </div>
       </div>
     </div>

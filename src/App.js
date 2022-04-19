3505e42b8f79bd54cdff9bfc7af649553b6f9d46
unknown <hussein-ayoub-96@hotmail.com>
2022-04-19 11:54:26 +0300

    some update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <VirtualTour />
+      {/* <VirtualTour /> */}
       <SliderNew />
       <Category />
       <Reviews />
